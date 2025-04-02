9a72b428dc5805a706f7f9a522667d0df80e5e01
Males24 <rafael.males03@gmail.com>
2025-04-02 16:47:20 +0100

    programa feito

--- PL03/Program.cs
+++ PL03/Program.cs
@@ -1,17 +1,53 @@
 ﻿using System;
 
-class Program
+public class Program
 {
     static void Main()
     {
-       Console.Write("base do retangulo ");
-       double baseValue = double.Parse(Console.ReadLine());
+        // Testes com a classe Rectangulo
+        Console.WriteLine("--- Testes com a classe Rectangulo ---");
+        try
+        {
+            Console.Write("Digite a base do retângulo: ");
+            double baseValue = double.Parse(Console.ReadLine());
 
-        Console.Write("altura do retangulo ");
-        double heightValue = double.Parse(Console.ReadLine());
-        Rectangulo rect = new Rectangulo(baseValue, heightValue);
-        //....
-        Console.WriteLine($"\nPerimetro: {rect.Perimetro()}");
-        Console.WriteLine($"Area: {rect.Area()}");
+            Console.Write("Digite a altura do retângulo: ");
+            double alturaValue = double.Parse(Console.ReadLine());
+
+            Rectangulo ret = new Rectangulo(baseValue, alturaValue);
+            Console.WriteLine($"\nRetângulo - Base: {ret.Base}, Altura: {ret.Altura}");
+            Console.WriteLine($"  Perímetro: {ret.Perimetro()}");
+            Console.WriteLine($"  Área: {ret.Area()}");
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("\nErro: Por favor, digite números válidos para a base e altura.");
+        }
+        catch (ArgumentException ex)
+        {
+            Console.WriteLine($"\nErro ao criar retângulo: {ex.Message}");
+        }
+
+        // Testes com a classe Quadrado
+        Console.WriteLine("\n--- Testes com a classe Quadrado ---");
+        try
+        {
+            Console.Write("Digite o lado do quadrado: ");
+            double ladoQuadrado = double.Parse(Console.ReadLine());
+
+            SquareClass quadrado = new SquareClass(ladoQuadrado);
+            Console.WriteLine($"\nQuadrado - Lado: {ladoQuadrado}");
+            Console.WriteLine($"  Perímetro: {quadrado.GetPerimeter()}");
+            Console.WriteLine($"  Área: {quadrado.GetArea()}");
+            Console.WriteLine($"  Diagonal: {quadrado.GetDiagonal()}");
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("\nErro: Por favor, digite um número válido para o lado do quadrado.");
+        }
+        catch (ArgumentException ex) // Se você adicionar validação na SquareClass
+        {
+            Console.WriteLine($"\nErro ao criar quadrado: {ex.Message}");
+        }
     }
 }
