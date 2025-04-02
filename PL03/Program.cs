fa2ec66247d81c7d0874470c1207f2b024800099
Rui Machado <74636066+RuiPVMachado@users.noreply.github.com>
2025-04-02 15:30:43 +0100

    Created RectangleClass
    Created a rectangle class for PL03 of PA

--- PL03/Program.cs
+++ PL03/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+       Console.Write("base do retangulo ");
+       double baseValue = double.Parse(Console.ReadLine());
+
+        Console.Write("altura do retangulo ");
+        double heightValue = double.Parse(Console.ReadLine());
+        Rectangulo rect = new Rectangulo(baseValue, heightValue);
+
+        Console.WriteLine($"\nPerimetro: {rect.Perimetro()}");
+        Console.WriteLine($"Area: {rect.Area()}");
+ 
+    }
+}
