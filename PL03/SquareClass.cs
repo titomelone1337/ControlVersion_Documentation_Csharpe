9947aa04f8c0d2642c59277984dfdb4fcab17325
andre-rosario-coelho <andre.rosario.coelho@gmail.com>
2025-04-09 15:56:31 +0100

    update_documentation_square

--- PL03/SquareClass.cs
+++ PL03/SquareClass.cs
@@ -1,24 +1,25 @@
-﻿/**
- * @class SquareClass
- * @brief Representa um quadrado e fornece métodos para calcular suas propriedades.
- */
+﻿/// <summary>
+/// Representa um quadrado e fornece métodos para calcular suas propriedades.
+/// </summary>
 public class SquareClass
 {
     private double _lado; ///< Armazena o valor do lado do quadrado.
 
-    /**
-     * @brief Construtor da classe SquareClass.
-     * @param l O comprimento do lado do quadrado.
-     */
+    /// <summary>
+    /// Inicializa uma nova instância da classe <see cref="SquareClass"/>.
+    /// </summary>
+    /// <param name="l">O comprimento do lado do quadrado.</param>
     public SquareClass(double l)
     {
         this._lado = l;
     }
 
-    /**
-     * @brief Propriedade para acessar e modificar o lado do quadrado.
-     * @throws ArgumentException Se o valor atribuído for menor ou igual a 0.
-     */
+    /// <summary>
+    /// Obtém ou define o comprimento do lado do quadrado.
+    /// </summary>
+    /// <exception cref="ArgumentException">
+    /// Lançado se o valor atribuído for menor ou igual a 0.
+    /// </exception>
     public double Lado
     {
         get => _lado;
@@ -30,21 +31,21 @@ public class SquareClass
         }
     }
 
-    /**
-     * @brief Calcula o perímetro do quadrado.
-     * @return O perímetro do quadrado.
-     */
+    /// <summary>
+    /// Calcula o perímetro do quadrado.
+    /// </summary>
+    /// <returns>O perímetro do quadrado.</returns>
     public double GetPerimeter() => _lado * 4;
 
-    /**
-     * @brief Calcula a área do quadrado.
-     * @return A área do quadrado.
-     */
+    /// <summary>
+    /// Calcula a área do quadrado.
+    /// </summary>
+    /// <returns>A área do quadrado.</returns>
     public double GetArea() => _lado * _lado;
 
-    /**
-     * @brief Calcula a diagonal do quadrado.
-     * @return O comprimento da diagonal do quadrado.
-     */
+    /// <summary>
+    /// Calcula o comprimento da diagonal do quadrado.
+    /// </summary>
+    /// <returns>O comprimento da diagonal do quadrado.</returns>
     public double GetDiagonal() => _lado * Math.Sqrt(2);
 }
